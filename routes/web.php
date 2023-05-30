2b8ffe4ef978c188fa552743a0e30254c548c47f
unknown <cclaudiaximena@gmail.com>
2023-05-30 16:38:04 -0500

    CreacionTablasCases

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AutorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
+
+
+
+
+Route::get('/autors', [AutorController::class, 'index'])->name('autors.index');
